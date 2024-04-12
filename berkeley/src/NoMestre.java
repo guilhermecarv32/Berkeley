46e545d5cbe357c3426bd229f9e5294a04d83d06
Guilherme Carvalho <guilhermecm1505@gmail.com>
2024-04-12 14:01:46 -0300

    Update NoMestre.java

--- berkeley/src/NoMestre.java
+++ berkeley/src/NoMestre.java
@@ -1,21 +1,15 @@
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class NoMestre {
-    private List<Sistema> sistemas;
-
-    public NoMestre() {
-        this.sistemas = new ArrayList<>();
+public class NoMestre extends Sistema {
+    public NoMestre(String nome, long horaAtual) {
+        super(nome, horaAtual);
     }
 
-    public void adicionarSistema(Sistema sistema) {
-        sistemas.add(sistema);
-    }
-
-    public void sincronizarRelogios() {
-        // Exibe a hora atual de cada sistema antes do ajuste
+    public void sincronizarRelogios(List<Sistema> sistemas) {
+        
+        // exibe a hora atual(simulada) formatada de cada sistema antes do ajuste
         System.out.println("Horas dos sistemas antes do ajuste:");
         for (Sistema sistema : sistemas) {
             SimpleDateFormat formatoHora = new SimpleDateFormat("HH:mm:ss");
@@ -23,24 +17,24 @@ public class NoMestre {
             System.out.println(sistema.getNome() + ": " + horaFormatada);
         }
 
-        // Separar no terminal as horas antes e depois do ajuste
+        // separar antes e depois no terminal
         System.out.println("-------------------");
 
         long somaTempos = 0;
 
-        // Calcula a soma dos tempos de todos os sistemas
+        // calcula a soma dos horarios de todos os sistemas
         for (Sistema sistema : sistemas) {
             somaTempos += sistema.getHoraAtual();
         }
 
-        // Calcula o tempo médio
+        // calcula o horario médio
         long tempoMedio = somaTempos / sistemas.size();
 
-        // Ajusta os relógios de acordo com a diferença em relação ao tempo médio
+        // ajusta os relógios
         for (Sistema sistema : sistemas) {
             long diferenca = tempoMedio - sistema.getHoraAtual();
             sistema.setHoraAtual(sistema.getHoraAtual() + diferenca);
-            // Exibe a hora formatada após o ajuste
+            //formata e exibe a hora depois do ajuste
             SimpleDateFormat formatoHora = new SimpleDateFormat("HH:mm:ss");
             String horaFormatada = formatoHora.format(new Date(sistema.getHoraAtual()));
             System.out.println("Relógio de " + sistema.getNome() + " ajustado para " + horaFormatada);
