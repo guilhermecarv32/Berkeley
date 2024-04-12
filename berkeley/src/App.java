22972dcf3cd801dc4ca8c03a89ab7969f4806b71
Guilherme Carvalho <guilhermecm1505@gmail.com>
2024-04-12 14:01:21 -0300

    Update App.java

--- berkeley/src/App.java
+++ berkeley/src/App.java
@@ -1,18 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Sistema sistemaA = new Sistema("Sistema A", 64800000); // Tempo inicial: 15 horas (em milissegundos) com ajuste de fuso horario (-3hrs)
-        Sistema sistemaB = new Sistema("Sistema B", 64200000); // Tempo inicial: 14 minutos e 50 minutos (em milissegundos) com ajuste de fuso horario (-3hrs)
-        Sistema sistemaC = new Sistema("Sistema C", 66300000); // Tempo inicial: 15 horas e 25 minutos (em milissegundos) com ajuste de fuso horario (-3hrs)
+        Sistema sistemaA = new NoMestre("Sistema A", 64800000); // Nome e Hora Atual: 15 horas (em milissegundos) com ajuste de fuso horario (-3hrs)
+        Sistema sistemaB = new Sistema("Sistema B", 64200000); // Nome e Hora Atual: 14 minutos e 50 minutos (em milissegundos) com ajuste de fuso horario (-3hrs)
+        Sistema sistemaC = new Sistema("Sistema C", 66300000); // Nome e Hora Atual: 15 horas e 25 minutos (em milissegundos) com ajuste de fuso horario (-3hrs)
 
-        // Criação do nó mestre
-        NoMestre noMestre = new NoMestre();
+        // criação do daemon de tempo
+        NoMestre NoMestre = (NoMestre) sistemaA;
 
-        // Adiciona os sistemas ao nó mestre
-        noMestre.adicionarSistema(sistemaA);
-        noMestre.adicionarSistema(sistemaB);
-        noMestre.adicionarSistema(sistemaC);
+        // lista dos sistemas
+        List<Sistema> sistemas = new ArrayList<>();
+        sistemas.add(sistemaA);
+        sistemas.add(sistemaB);
+        sistemas.add(sistemaC);
 
-        // Sincroniza os relógios
-        noMestre.sincronizarRelogios();
+        // sincroniza os relógios
+        NoMestre.sincronizarRelogios(sistemas);
     }
 }
