ffc704bf6bfe80ee6f3da07656d398f6ad2fd680
Guilherme Carvalho <guilhermecm1505@gmail.com>
2024-04-12 23:59:34 -0300

    Update App.java

--- berkeley/src/App.java
+++ berkeley/src/App.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
         Sistema sistemaA = new NoMestre("Sistema A", 64800000); // Nome e Hora Atual: 15 horas (em milissegundos) com ajuste de fuso horario (-3hrs)
-        Sistema sistemaB = new Sistema("Sistema B", 64200000); // Nome e Hora Atual: 14 minutos e 50 minutos (em milissegundos) com ajuste de fuso horario (-3hrs)
+        Sistema sistemaB = new Sistema("Sistema B", 64200000); // Nome e Hora Atual: 14 horas e 50 minutos (em milissegundos) com ajuste de fuso horario (-3hrs)
         Sistema sistemaC = new Sistema("Sistema C", 66300000); // Nome e Hora Atual: 15 horas e 25 minutos (em milissegundos) com ajuste de fuso horario (-3hrs)
 
         // criação do daemon de tempo
